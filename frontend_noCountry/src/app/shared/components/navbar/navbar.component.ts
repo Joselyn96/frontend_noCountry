a20b6c8ec0bad3677c5f9b6c400e96f9e5352b82
Joselyn96 <joselynvalverde6916@gmail.com>
2025-10-20 17:48:22 -0500

    feat(dashboard): add admin dashboard layout and tabs

---
+++ frontend_noCountry/src/app/shared/components/navbar/navbar.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+  @Input() panelTitle: string = 'Panel de Administración'; // texto por defecto
+  @Input() userRole: string = 'Admin'; // rol del usuario
+  @Input() userSubtitle: string = 'Administrador'; // subtitulo del usuario
+
+  logout() {
+    // logout
+    console.log('Logout');
+  }
+}
